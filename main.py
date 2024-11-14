d8ddc00d86a88a5a324fa3895f08891bb8eea1b2
Hugo <551108@student.saxion.nl>
2024-11-14 10:37:35 +0100

    added gitignore file

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from game_objects.locations import Location
 
 def main():
     floor_1 = Location(starting_x=0, starting_y=0)
-    player = Player(name="player", health=100, location=Location)
+    player = Player(name="player", health=100, location=floor_1)
 
 
 if __name__ == "__main__":
     main()
+    print("you enter the building")
