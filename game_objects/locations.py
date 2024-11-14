1af82948bdb02e43e1c787c965a1cc1e6711dc02
Robin Camfferman <482278@student.saxion.nl>
2024-11-14 11:09:18 +0100

    Added basic Location functions

--- game_objects/locations.py
+++ game_objects/locations.py
@@ -1,1 +1,19 @@
-x = 5
+class Location:
+    def __init__(self, starting_x=0, starting_y=0):
+        self.x = starting_x
+        self.y = starting_y
+
+    def get_location(self):
+        return self.x, self.y
+
+    def move_left(self, amount=1):
+        self.x -= amount
+
+    def move_right(self, amount=1):
+        self.x += amount
+
+    def move_up(self, amount=1):
+        self.y += amount
+
+    def move_down(self, amount=1):
+        self.y -= amount
