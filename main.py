0bdf4eb00720c5b7d0bfd8de81ad707fc3198056
Hugo <551108@student.saxion.nl>
2024-11-14 10:44:52 +0100

    added game_objects to main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from game_objects.player import Player
 from game_objects.locations import Location
+from game_objects.items import Item, Weapon, Armor, Key
 
 
 def main():
     floor_1 = Location(starting_x=0, starting_y=0)
-    player = Player(name="player", health=100, location=floor_1)
+    player = Player(name="notDemonFrank", health=100, location=floor_1)
+    print("you enter the building")
+    print(player)
 
 
 if __name__ == "__main__":
     main()
-    print("you enter the building")
