3d01268754d318a887fe2535da81c137f17737af
Yirnick van Dijk <71263820+NitraatPlagiaat@users.noreply.github.com>
2024-11-14 12:45:52 +0100

    Demons so far so good

--- game_objects/demon.py
+++ game_objects/demon.py
@@ -1,38 +1,61 @@
 from abc import ABC, abstractmethod
 from demon_attacks import attacks
+import random
+
 
 class Demon(ABC):
     def __init__(self):
         self.health = 100
-        self.image = ''
-        self.attacks = {}
-
-
-    @abstractmethod
-    def set_damage(self):
-        pass
+        self.attacks = attacks
 
     def get_damage(self):
         return self.health
+
+    @abstractmethod
+    def set_damage(self, damage):
+        pass
+
+    @abstractmethod
+    def attack(self):
+        pass
 
 
 class Frank(Demon):
     def __init__(self):
         super().__init__()
         self.image = '/images/demons/frank.jpg'
-        self.attacks = attacks["frank"]
 
     def set_damage(self, damage):
-        self.health -= damage - 70
+        self.health -= damage * 0.3
 
+    def attack(self):
+        while True:
+            attack = random.choice(list(attacks.items()))
+            if attack[1]["damage"] >= 30 and attack[1]["Damage"] <= 35:
+                break
+        return attack
+            
 
 class Tibor(Demon):
     def __init__(self):
         super().__init__()
         self.image = '/images/demons/tibor.jpg'
+        self.custom_attacks = {
+            "Insults you in Swedish": {
+                "Message": "Din dator är en potatis för helvete.", "Damage": 18
+                }
+            }
 
     def set_damage(self, damage):
-        self.health -= damage - 20
+        self.health -= damage * 0.8
+
+    def attack(self):
+        attack_type = random.choice([attacks.items(), self.custom_attacks.items()])
+        while True:
+            attack = random.choice(list(attack_type))
+            if attack[1]["Damage"] >= 15 and attack[1]["Damage"] <= 20:
+                break
+        return attack
 
 
 class Peter(Demon):
@@ -41,7 +64,14 @@ class Peter(Demon):
         self.image = '/images/demons/peter.jpg'
 
     def set_damage(self, damage):
-        self.health -= damage - 15
+        self.health -= damage * 0.8
+
+    def attack(self):
+        while True:
+            attack = random.choice(list(attacks.items()))
+            if attack[1]["Damage"] >= 20 and attack[1]["Damage"] <= 25:
+                break
+        return attack
 
 
 class Olivier(Demon):
@@ -50,4 +80,14 @@ class Olivier(Demon):
         self.image = '/images/demons/olivier.jpg'
 
     def set_damage(self, damage):
-        self.health -= damage - 30
+        self.health -= damage * 0.7
+
+    def attack(self):
+        while True:
+            attack = random.choice(list(attacks.items()))
+            if attack[1]["Damage"] >= 25 and attack[1]["Damage"] <= 30:
+                break
+        return attack
+
+demon = Tibor()
+demon.attack()
