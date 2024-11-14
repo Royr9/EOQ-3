bf30a873a3d476ed1b950b948e19dc24e5b756b7
Hugo <551108@student.saxion.nl>
2024-11-14 10:26:23 +0100

    added basic code to main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# main.py
+from game_objects.player import Player
+from game_objects.locations import Location
+
+
+def main():
+    floor_1 = Location(starting_x=0, starting_y=0)
+    player = Player(name="player", health=100, location=Location)
+
+
+if __name__ == "__main__":
+    main()
