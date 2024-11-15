3f3f574c8fce73ce4274c4cc47c07af9122a915a
Robin Camfferman <482278@student.saxion.nl>
2024-11-15 13:37:53 +0100

    Hotfix

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def choose_action():
     action_name = request.form.get('action')
     location = game_object.get_current_level().get_current_location()
 
-    if not game_object.get_current_level().is_demon_killed() and "Fight" in action_name:
+    if not game_object.get_current_level().is_demon_killed() and "Fight" in action_name or "Disturb" in action_name:
         demon = get_demon_by_name(location.demon)
         return redirect(url_for("fight"))
 
